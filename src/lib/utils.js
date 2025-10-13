97bc81e51e6947b53cf537c1f11ddd0ccc31455c
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-10-13 16:00:35 +0700

    add shadcn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,10 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
 export const formatMenuDate = (raw) => {
   let date;
   if (!raw) {
